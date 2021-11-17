0a973a5dd11799074caf63553d7573762e889080
Guillermo Tanamachi <A01631327@itesm.mx>
2021-11-16 23:12:20 -0600

    Master test

--- index.py
+++ index.py
@@ -1,18 +1,33 @@
 from mpi4py import MPI
 
-matriz = [
+matrix = [
     [1, 2, 3, 4],
     [4, 5, 6, 7],
     [8, 3, 6, 8],
     [8, 3, 6, 8]
 ]
 
-r = MPI.COMM_WORLD.Get_rank()
-s = MPI.COMM_WORLD.Get_size()
+comm = MPI.COMM_WORLD
+r = comm.Get_rank()
+s = comm.Get_size()
 
 if r == 0:
-    for i in range(1,s+1):
-        print(i)
+    per_rank = len(matrix)//s
+    more = len(matrix)%s
+    start = 0
+    for dest in range(1,s):
+        rows = 0
+        if dest <= more:
+            rows = per_rank + 1
+        else:
+            rows = per_rank
+        comm.send({rows:rows, start:start}, dest=dest)
+        start += rows
+else:
+    m = comm.recv()
+    print(f"Message received: Rows {m['rows']}, Start: {m['start']}")
+
+
 
 
 print(f"Desde el rank {r}, size: {s}")
