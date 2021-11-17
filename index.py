8bf2d6f984558382262ca62769ef4348dc3260d7
Guillermo Tanamachi <A01631327@itesm.mx>
2021-11-16 22:57:32 -0600

    Size test

--- index.py
+++ index.py
@@ -1,6 +1,18 @@
 from mpi4py import MPI
 
-r = MPI.COMM_WORLD.Get_rank()
-n = MPI.Get_processor_name()
+matriz = [
+    [1, 2, 3, 4],
+    [4, 5, 6, 7],
+    [8, 3, 6, 8],
+    [8, 3, 6, 8]
+]
 
-print(f"Desde el rank {r}  en {n}")
+r = MPI.COMM_WORLD.Get_rank()
+s = MPI.COMM_WORLD.Get_size()
+
+if r == 0:
+    for i in range(1,s+1):
+        print(i)
+
+
+print(f"Desde el rank {r}, size: {s}")
